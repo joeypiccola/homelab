5e9661073fdff0ee0cc681f7af83cd467a23db53
Joey Piccola <joey.piccola@gmail.com>
2020-07-09 15:56:34 -0600

    more ssh

--- packer/scripts/lin/cleanup.sh
+++ packer/scripts/lin/cleanup.sh
@@ -3,4 +3,18 @@
 set -e
 set -x
 
-sudo apt-get clean
+echo "==> Cleaning up tmp"
+sudo rm -rf /tmp/*
+
+echo "==> Cleaning up apt"
+sudo apt-get -y autoremove --purge
+sudo apt-get -y clean
+sudo apt-get -y autoclean
+
+echo "==> Cleaning up udev rules"
+sudo rm -rf /dev/.udev/
+
+echo "==> Blanking systemd machine-id"
+if [ -f "/etc/machine-id" ]; then
+    sudo truncate -s 0 "/etc/machine-id"
+fi
