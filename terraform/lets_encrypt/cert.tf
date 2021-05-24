2246b414803fde9f73e9ac9c8500d66ac40ec057
jpiccola <joey.piccola@gmail.com>
2021-05-24 00:42:01 -0600

    feat: lets encrypt init

---
+++ terraform/lets_encrypt/cert.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    acme = {
+      source = "vancluever/acme"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "acme" {
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+}
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+}
+
+resource "acme_registration" "reg" {
+  account_key_pem = "${tls_private_key.private_key.private_key_pem}"
+  email_address   = "joey.piccola@gmail.com"
+}
+
+resource "acme_certificate" "certificate" {
+  account_key_pem           = "${acme_registration.reg.account_key_pem}"
+  common_name               = "web-a.joeypiccola-aws.com"
+  subject_alternative_names = ["weba-a.joeypiccola-aws.com"]
+
+  dns_challenge {
+    provider = "route53"
+  }
+}
+
+resource "aws_acm_certificate" "cert" {
+  private_key      = acme_certificate.certificate.private_key_pem
+  certificate_body = acme_certificate.certificate.certificate_pem
+  certificate_chain = "${acme_certificate.certificate.certificate_pem}${acme_certificate.certificate.issuer_pem}"
+}
+
+output "cert" {
+  value = aws_acm_certificate.cert.arn
+}
