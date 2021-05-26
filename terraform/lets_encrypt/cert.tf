c10c1c7506885f077285cbfb819db6f95768ea11
jpiccola <joey.piccola@gmail.com>
2021-05-25 20:31:17 -0600

    style: mv provider

--- terraform/lets_encrypt/cert.tf
+++ terraform/lets_encrypt/cert.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    acme = {
-      source = "vancluever/acme"
-      version = "2.4.0"
-    }
-  }
-}
-
-provider "acme" {
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-}
-
 resource "tls_private_key" "private_key" {
   algorithm = "RSA"
 }
--- terraform/lets_encrypt/provider.tf
+++ terraform/lets_encrypt/provider.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-west-1"
 }
+
+provider "acme" {
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+}
+
+terraform {
+  required_providers {
+    acme = {
+      source = "vancluever/acme"
+      version = "2.4.0"
+    }
+  }
+}
