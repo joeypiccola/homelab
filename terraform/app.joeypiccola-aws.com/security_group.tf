21f46b8df772e55c82dca6b910b713c12a398940
jpiccola <joey.piccola@gmail.com>
2021-05-25 20:32:36 -0600

    style: t fmt fixes

--- terraform/app.joeypiccola-aws.com/security_group.tf
+++ terraform/app.joeypiccola-aws.com/security_group.tf
@@ -1,6 +1,6 @@
 variable "sg_allowed_ports" {
-    type = list(number)
-    default = [80,22,443]
+  type    = list(number)
+  default = [80, 22, 443]
 }
 
 resource "aws_security_group" "sg" {
@@ -9,20 +9,20 @@ resource "aws_security_group" "sg" {
     iterator = port
     for_each = var.sg_allowed_ports
     content {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      protocol = "TCP"
-      from_port = port.value
-      to_port = port.value
+      cidr_blocks = ["0.0.0.0/0"]
+      protocol    = "TCP"
+      from_port   = port.value
+      to_port     = port.value
     }
   }
   dynamic "egress" {
     iterator = port
     for_each = var.sg_allowed_ports
     content {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      protocol = "TCP"
-      from_port = port.value
-      to_port = port.value
+      cidr_blocks = ["0.0.0.0/0"]
+      protocol    = "TCP"
+      from_port   = port.value
+      to_port     = port.value
     }
   }
 }
